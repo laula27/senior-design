24f91fdb29310f9bc7ffcabec2fd6cef23ce98de
laula27 <77760870+laula27@users.noreply.github.com>
2022-02-10 21:05:03 -0500

    Delete Debug directory

--- Debug/src/ff.d
+++
@@ -1,9 +0,0 @@
-src/ff.o: ../src/ff.c /Users/yining/Documents/workspace/UART_LED/inc/ff.h \
- /Users/yining/Documents/workspace/UART_LED/inc/ffconf.h \
- /Users/yining/Documents/workspace/UART_LED/inc/diskio.h
-
-/Users/yining/Documents/workspace/UART_LED/inc/ff.h:
-
-/Users/yining/Documents/workspace/UART_LED/inc/ffconf.h:
-
-/Users/yining/Documents/workspace/UART_LED/inc/diskio.h:
--- Debug/src/fifo.d
+++
@@ -1,4 +0,0 @@
-src/fifo.o: ../src/fifo.c \
- /Users/yining/Documents/workspace/UART_LED/inc/fifo.h
-
-/Users/yining/Documents/workspace/UART_LED/inc/fifo.h:
